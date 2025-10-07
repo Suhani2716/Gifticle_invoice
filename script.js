7417de6f5287cb8cd98a81c5a499b6ff298dada0
Suhani2716 <suhani.k1@ahduni.edu.in>
2025-10-07 09:41:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -90,6 +90,8 @@ function convertToWords(num) {
     if (n < 100) return b[Math.floor(n / 10)] + (n % 10 ? " " + a[n % 10] : "");
     if (n < 1000) return a[Math.floor(n / 100)] + " hundred" + (n % 100 ? " " + inWords(n % 100) : "");
     if (n < 100000) return inWords(Math.floor(n / 1000)) + " thousand" + (n % 1000 ? " " + inWords(n % 1000) : "");
+    if (n < 10000000) return inWords(Math.floor(n / 100000)) + " lakh" + (n % 100000 ? " " + inWords(n % 100000) : "");
+    if (n < 1000000000) return inWords(Math.floor(n / 10000000)) + " crore" + (n % 10000000 ? " " + inWords(n % 10000000) : "");
     return "amount too large";
   }
 
